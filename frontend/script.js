e3f0119b7761dcc72a788f432f9ab5b287c1caf0
edwinvg99 <velasquezgiraldoedwin@gmail.com>
2025-06-14 03:20:57 -0500

    Actualizacion de dominio de vercel para despliegue

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 
-const BACKEND_URL = 'http://localhost:3001'; 
+const BACKEND_URL = 'https://alertas-maps.vercel.app/'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     initializeApp();
